7650a1315999d7091bfe1082e7ffe671fae8d081
waed <waed.khalifa@outlook.com>
2020-09-29 20:00:32 +0300

    Edit class Amenities(added three of amenities)

--- src/main/java/najah/edu/app/Amenities.java
+++ src/main/java/najah/edu/app/Amenities.java
@@ -4,6 +4,9 @@ public class Amenities {
 	public static final String ELEVATOR = "ELEVATOR";
 	public static final String FIREPLACE = "FIREPLACE";
 	public static final String GARAGEPARKING = "GARAGEPARKING";
+	public static final String AIRCONDITIONING = "AIRCONDITIONING";
+	public static final String BALCONY = "BALCONY";
+	public static final String SWIMMINGPOOL = "SWIMMINGPOOL";
 
   
 private String amenities;
